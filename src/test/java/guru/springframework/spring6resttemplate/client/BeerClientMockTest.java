e3bcfd15c173dbb3b7f716d315457c20ca6ec305
mmedic-git <midosdking@gmail.com>
2024-03-13 12:39:12 +0100

    Section 20  - Spring RestTemplate nastavak

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -19,15 +19,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withAccepted;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 
@@ -55,14 +57,54 @@ public class BeerClientMockTest {
     @Mock
     RestTemplateBuilder mockRestTemplateBuilder = new RestTemplateBuilder((new MockServerRestTemplateCustomizer()));
 
-    @BeforeEach
-    void setUp() {
+    BeerDTO dto;
+    String dtoJson;
+
+    @BeforeEach // ZAPAMTI: ovaj kod će se izvesti prije svake testne metode u ovom unit-u
+    void setUp() throws JsonProcessingException {
         RestTemplate restTemplate = restTemplateBuilderConfigured.build();
         server = MockRestServiceServer.bindTo(restTemplate).build(); //bindamo server i pridružujemo server instance, radije nego kao prije, da Spring Context to radi
 
-        when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);
+        when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);  //želimo vratiti restTemplate objekt koji je boundan na server
         beerClient = new BeerClientImpl(mockRestTemplateBuilder);
 
+        dto = getBeerDto();   //obzirom da je getBeerDto() već praktički izgenerirao UUID, imam sve što mi treba za get by  .id(UUID.randomUUID())
+
+        dtoJson = objectMapper.writeValueAsString(dto);   // buffer za JSON string koji ćemo primiti nazad
+
+
+    }
+
+    @Test
+    void testCreateBeer() {
+
+        URI uri = UriComponentsBuilder.fromPath(BeerClientImpl.GET_BEER_BY_ID_PATH).build(dto.getId());
+
+        server.expect(method(HttpMethod.POST))
+                        .andExpect(requestTo(URL + BeerClientImpl.GET_BEER_PATH))
+                                .andRespond(withAccepted().location(uri));
+
+        server.expect(method(HttpMethod.GET))
+                .andExpect(requestToUriTemplate(URL + BeerClientImpl.GET_BEER_BY_ID_PATH, dto.getId()))
+                .andRespond(withSuccess(dtoJson, MediaType.APPLICATION_JSON));
+
+        BeerDTO responseDTO = beerClient.createBeer(dto);
+
+        assertThat(responseDTO.getId()).isEqualTo(dto.getId());
+
+
+    }
+
+    @Test
+    void testGetById()  {
+
+        server.expect(method(HttpMethod.GET))
+                .andExpect(requestToUriTemplate(URL + BeerClientImpl.GET_BEER_BY_ID_PATH, dto.getId()))
+                .andRespond(withSuccess(dtoJson, MediaType.APPLICATION_JSON));
+
+        BeerDTO responseDTO = beerClient.getBeerById(dto.getId());  // ako je test uspješan, znači da smo uspješno dobili json, koji se uspješno konvertira u DTO objekt...
+
+        assertThat(responseDTO.getId()).isEqualTo(dto.getId());  // i id-evi dohvačenog responseDTO i "originalnog" DTO objekta su jednaki
 
     }
 
@@ -70,6 +112,7 @@ public class BeerClientMockTest {
     void testListBeers() throws JsonProcessingException {
         String payload = objectMapper.writeValueAsString(getPage());
 
+        //dajemo naredbu Mockito-u da izvrši poziv GET API-ja, da pozove glavni URL, i da server odgovori sa payloadom tipa JSON
         server.expect(method(HttpMethod.GET))
                 .andExpect(requestTo(URL + BeerClientImpl.GET_BEER_PATH))
                 .andRespond(withSuccess(payload, MediaType.APPLICATION_JSON));
